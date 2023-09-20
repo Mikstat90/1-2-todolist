16402b074ed9a1499975a0c43579483ced52ca38
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-09-20 14:49:00 +0200

    function for button

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,37 @@ export type ButtonNameType = 'All' | 'Active' | 'Completed'
 
 function App() {
 
-    // let tasks = [
-    //     {id: 1, title: "HTML&CSS", isDone: true},
-    //     {id: 2, title: "JS", isDone: true},
-    //     {id: 3, title: "ReactJS", isDone: false}
-    // ]
-
     let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
     ])
 
+    let [filteredTasks, setFilteredTasks] = useState<ButtonNameType>('All')
+
     const removeTask = (id: number) => {
-        // tasks = tasks.filter(el => el.id !== id)
-        // setTasks(tasks)
         setTasks(tasks.filter(el => el.id !== id))
     }
 
     const filterTasks = (buttonName: ButtonNameType) => {
+        setFilteredTasks(buttonName)
+    }
 
-        if (buttonName === 'Active') {
-            setTasks(tasks.filter(el => !el.isDone))
-        }
-        if (buttonName === 'Completed') {
-            setTasks(tasks.filter(el => el.isDone))
-        }
-        if (buttonName === 'All') {
-            setTasks(tasks)
-        }
+    let durshlag = tasks
+    if (filteredTasks === 'Active') {
+        durshlag = tasks.filter(el => !el.isDone)
+    }
+    if (filteredTasks === 'Completed') {
+        durshlag = tasks.filter(el => el.isDone)
     }
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks} removeTask={removeTask} filterTasks={filterTasks}/>
+            <Todolist
+                title="What to learn"
+                tasks={durshlag}
+                removeTask={removeTask}
+                filterTasks={filterTasks}/>
         </div>
     );
 }
